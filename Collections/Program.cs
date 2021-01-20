f887eaab26a1b4cf9acbecd6c527cb21a32d648b
ykizilirmak <ykizilirmak93@gmail.com>
2021-01-20 23:50:42 +0300

    20 Ocak Ders Sonu

---
+++ Collections/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] names = new string[]{"ali","ahmet"};
+            //Console.WriteLine(names[0]);
+            //Console.WriteLine(names[1]);
+
+            //names = new string[3];
+            //names[2] = "yusuf";
+            //Console.WriteLine(names[2]);
+            //Console.WriteLine(names[1]);
+
+            List<string> names2 = new List<string> { "ali", "ahmet" };
+            Console.WriteLine(names2[0]);
+            Console.WriteLine(names2[1]);
+            names2.Add("yusuf");
+            Console.WriteLine(names2[2]);
+            Console.WriteLine(names2[1]);
+
+
+
+
+
+        }
+    }
+}
---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> myList= new MyList<string>();
+            myList.Add("yusuf");
+          //  Console.WriteLine(myList[0]);
+
+
+        }
+    }
+}
